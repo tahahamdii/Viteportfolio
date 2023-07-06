5497178ac4944bdfd2810f48d2b2a4ed6dfefe3e
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-07-06 02:28:49 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import '../styles/style.css'
+import '../styles/modern-normalize.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/featured.css';
+import '../styles/components/work.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+import lazyLoading from './utils/lazy-loading';
+
+mobileNav();
+darkMode();
+lazyLoading();
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
